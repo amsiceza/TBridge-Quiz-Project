8dce23f2c624bdf788df853eb9f151e666b0d310
[amsiceza] <[ismacerverazamora@gmail.com]>
2023-03-15 13:13:36 +0100

    Comments main.js

--- scripts/main.js
+++ scripts/main.js
@@ -15,8 +15,8 @@ const API_URL = "https://opentdb.com/api.php?amount=10&category=21&difficulty=ea
 // Variable de datosQuiz = []
     // --> Guarda datos de la api (con axios).
     // --> Sobre esta variable sacamos el contenido.
-    const datosQuiz = []
-    console.log(datosQuiz)
+        const datosQuiz = []
+        console.log(datosQuiz)
 
 // LLamada a la API mediante axios.
     // --> En el .then guardaremos los datos en una variable creada ".datosQuiz"
@@ -45,7 +45,6 @@ const API_URL = "https://opentdb.com/api.php?amount=10&category=21&difficulty=ea
             cardQuiz.classList.add("hide");
             restartButton.classList.add("hide")
             currentQuestionIndex = 0;
-            countCorrect = 0;
             questionContainerElement.classList.remove("hide");
             setNextQuestion()
         }
@@ -59,86 +58,100 @@ const API_URL = "https://opentdb.com/api.php?amount=10&category=21&difficulty=ea
         
         function showQuestion(question) {
 
-            // pinta la pregunta
+            // Pinta la pregunta
             questionElement.innerHTML = question.question
             // Crea un arreglo con todas las respuestas (correctas e incorrectas)
             const answers = [...question.incorrect_answers, question.correct_answer]
             // Mezcla las respuestas utilizando sort() y Math.random()
             const shuffledAnswers = answers.sort(() => Math.random() - 0.5);
-            
             // Pinta los botones de respuesta en orden aleatorio
             shuffledAnswers.forEach(answer => {
-            const button = document.createElement("button");
-            button.id = "answerButton"
-            button.innerText = answer;
-            answerButtonsElement.appendChild(button);
-            button.addEventListener("click",()=>{
-                selectAnswer()
-                if(button.dataset.correct){
-                    countCorrect++
-                }    
-                console.log(countCorrect);       
-            } );
+                const button = document.createElement("button");
+                button.id = "answerButton"
+                button.innerText = answer;
+                answerButtonsElement.appendChild(button);
 
+            // Funcion  que se le añade al boton de cada una de las respuestas
+                // --> Contador a la variable de correctas
+                button.addEventListener("click",()=>{
+                    selectAnswer()
+                        if(button.dataset.correct){
+                            countCorrect++
+                        }    
+                    console.log(countCorrect);       
+                });
             });
-
         }
 
-    function setNextQuestion() {
-        resetState();
-        showQuestion(datosQuiz[currentQuestionIndex]);
-    }
+// Funcion setNextQuestion() aplicada en la funcion startGame
+    // --> Elimina el contenido del contenedor de botones 
+    // --> Muestra el contenido de la siguiente pregunta
+        function setNextQuestion() {
+            resetState();
+            showQuestion(datosQuiz[currentQuestionIndex]);
+        }
+// Funcion resetState() va dentro de setNextQuestion
+    // --> Esconde el boton de nextButton - Siguiente pregunta
+    // --> Mientras haya hijo en botones lo elimina
+        function resetState() {
+            nextButton.classList.add("hide");
+            while (answerButtonsElement.firstChild) {
+                answerButtonsElement.removeChild(answerButtonsElement.firstChild);
+            }
+        }   
+// Funcion para el boton de siguiente pregunta
+    // --> Se le añade uno al contador numero pregunta
+    // --> Se ejecuta la funcion setNextQuestion
+        nextButton.addEventListener("click", () => {
+            currentQuestionIndex++;
+            setNextQuestion();
+        });
 
 
 // Variables para guardar en diferentes arrays las respuestas correctas e incorrectas
         let answerFalse = []
         let answerTrue = []
 
-// Funcion para chekear las respuestas
+// Funcion para chekear las respuestas (se añande en selectAnswer)
     // --> Se implementa en la función showQuestion
-function checkAnswer(button) {
-    answerFalse = datosQuiz[currentQuestionIndex].incorrect_answers
-    answerTrue = datosQuiz[currentQuestionIndex].correct_answer
+    // --> Añade clases de correcta o erronea para pintar
+        function checkAnswer(button) {
+            answerFalse = datosQuiz[currentQuestionIndex].incorrect_answers
+            answerTrue = datosQuiz[currentQuestionIndex].correct_answer
 
-        if(answerFalse.includes(button.innerText)){
-            button.classList.add("erronea")
-            button.disabled = true
-        
-        }else{
-            button.classList.add("correcta")
-            button.disabled = true 
-            button.dataset.correct = true
+                if(answerFalse.includes(button.innerText)){
+                    button.classList.add("erronea")
+                    button.disabled = true
+                
+                }else{
+                    button.classList.add("correcta")
+                    button.disabled = true 
+                    button.dataset.correct = true
+                }
         }
-}
 
-function selectAnswer() {
-    
-    Array.from(answerButtonsElement.children).forEach((button) => {
-        checkAnswer(button);
-    }); 
+// Funcion al seleccionar respuest (se añade a botones de respuesta)
+    // --> Recorre cada boton de respuesta y checkAnswer()
+    // --> Comprueba que se hayam recorrido todas las preguntas
+        function selectAnswer() {
+            Array.from(answerButtonsElement.children).forEach((button) => {
+                checkAnswer(button);
+            }); 
 
-        if (datosQuiz.length > currentQuestionIndex + 1) {
-            nextButton.classList.remove("hide");
-        } else {
-            restartButton.classList.remove("hide");
-        } 
-}
-
-
-
+                if (datosQuiz.length > currentQuestionIndex + 1) {
+                    nextButton.classList.remove("hide");
+                } else {
+                    restartButton.classList.remove("hide");
+                } 
+        }
 
 
-nextButton.addEventListener("click", () => {
-    currentQuestionIndex++;
-    setNextQuestion();
-});
 
-function resetState() {
-    nextButton.classList.add("hide");
-    while (answerButtonsElement.firstChild) {
-        answerButtonsElement.removeChild(answerButtonsElement.firstChild);
-    }
-}
+
+
+
+
+
 
 
     
